72b0df7c33555a23eee1753ca07883a8bd2fc255
hueyhuilonghan <hueyhuilonghan@users.noreply.github.com>
2023-09-03 17:16:26 -0700

    Synchronizing job iceberg_demo from AWS Glue at 2023/09/04 00:16:26

---
+++ iceberg_demo/iceberg_demo.py
@@ -0,0 +1,28 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+## @params: [JOB_NAME]
+args = getResolvedOptions(sys.argv, ['JOB_NAME', 'iceberg_job_catalog_warehouse'])
+conf = SparkConf()
+
+## Please make sure to pass runtime argument --iceberg_job_catalog_warehouse with value as the S3 path 
+conf.set("spark.sql.catalog.job_catalog.warehouse", args['iceberg_job_catalog_warehouse'])
+conf.set("spark.sql.catalog.job_catalog", "org.apache.iceberg.spark.SparkCatalog")
+conf.set("spark.sql.catalog.job_catalog.catalog-impl", "org.apache.iceberg.aws.glue.GlueCatalog")
+conf.set("spark.sql.catalog.job_catalog.io-impl", "org.apache.iceberg.aws.s3.S3FileIO")
+conf.set("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")
+conf.set("spark.sql.source.partitionOverwriteMode", "dynamic")
+conf.set("spark.sql.iceberg.handle-timestamp-without-timezone", "true")
+
+sc = SparkContext(conf=conf)
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+
+job.commit()
